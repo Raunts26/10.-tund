42ce2218cb18bdec79710404b2facfc806350dc6
Rauno Kosula <raunkos@greeny.cs.tlu.ee>
2015-11-09 13:00:04 +0200

    Interest class

---
+++ InterestManager.class.php
@@ -0,0 +1,21 @@
+<?php
+class InterestManager {
+	
+	private $connection;
+	private $user_id;
+	
+	function __construct($mysqli, $user_id) {
+		$this->connection = $mysqli;
+		$this->user_id = $user_id;
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+}
+?>
