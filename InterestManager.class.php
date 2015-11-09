db8a1da131aa4864bd5af24e0408e17376a4c594
Rauno Kosula <raunkos@greeny.cs.tlu.ee>
2015-11-09 14:15:48 +0200

    dropdown menu

--- InterestManager.class.php
+++ InterestManager.class.php
@@ -8,9 +8,78 @@ class InterestManager {
 		$this->connection = $mysqli;
 		$this->user_id = $user_id;
 		
-		
 	}
 	
+	function addInterest($int_name) {
+		
+		$response = new StdClass();
+		
+		//Kontrollid, et sellist veel ei ole?
+		$stmt = $this->connection->prepare("SELECT id FROM interests WHERE name = ?");
+        $stmt->bind_param("s", $int_name);
+        $stmt->bind_result($id);
+        $stmt->execute();
+		
+		if($stmt->fetch()){
+            $error = new StdClass();
+            $error->id = 0;
+            $error->message = "Selline huvi on juba olemas";
+            $response->error = $error;
+            return $response;
+        }
+		$stmt->close();
+		
+        $stmt = $this->connection->prepare("INSERT INTO interests (name) VALUES (?)");
+        $stmt->bind_param("s", $int_name);
+        if($stmt->execute()){
+			
+            $success = new StdClass();
+            $success->message = "Huvi edukalt loodud";
+            
+            $response->success = $success;
+			
+            
+        }else{
+            $error = new StdClass();
+            $error->id = 1;
+            $error->message = "Midagi läks katki";
+            
+            $response->error = $error;
+        }
+		
+		return $response;
+		
+		$stmt->close();
+		
+		
+
+	}
+	
+	function createDropdown() {
+		
+		$html = '';
+		//Liidan eelmisele juurde
+		$html .= '<select name="dropdown_interest">';
+		
+		$stmt = $this->connection->prepare("SELECT id, name FROM interests");
+		$stmt->bind_result($id, $name);
+		$stmt->execute();
+		//Iga rea kohta
+		while($stmt->fetch()) {
+			
+			$html .= '<option>'.$name.'</option>';
+			
+		}
+		
+		$stmt->close();
+		#$html .= '<option>Test 1</option>';
+		#$html .= '<option selected>Test 2</option>';
+		
+		$html .= '</select>';
+		
+		return $html;
+		
+	}
 	
 	
 	
--- data.php
+++ data.php
@@ -12,11 +12,26 @@
     if(isset($_GET["logout"])){
         session_destroy();
         header("Location: login.php");
+		exit();
     }
+	
+	
+
 	
 	##HALDUS##
 	
-	$InterestManager = new InterestManager($mysqli, $_SESSION['id_from_db']{
+	$InterestManager = new InterestManager($mysqli, $_SESSION['user_id']);
+	
+	if(isset($_GET["insert"])){
+		$int_name = $_GET["insert"];
+		$InterestManager->addInterest($int_name);
+		$interest_response = $InterestManager->addInterest($int_name);
+	}
+
+
+
+	
+	
 
 ?>
 
@@ -36,7 +51,32 @@ Tere, <?=$_SESSION['user_email'];?> <a href="?logout=1">Logi välja</a>
 endif; ?>
 
 
+<?php if(isset($interest_response->success)): ?>
 
+<p style="color:green;">
+<?=$interest_response->success->message;?>
+</p>
+
+<?php elseif(isset($interest_response->error)): ?>
+
+<p style="color:red;">
+<?=$interest_response->error->message;?>
+</p>
+
+<?php endif; ?>
+
+<h2>Lisa uus</h2>
+<form method="get">
+
+<input name="insert" type="text" placeholder="Huvi...">
+<input type="submit" name="create" value="Loo huvi">
+
+</form>
+
+<h2>Minu huvialad</h2>
+
+
+<?=$InterestManager->createDropdown();?>
 
 
 
